7140e7da98bf977bfb55a54098d9416d386b474e
succesbusinesspro-ux <succes.businesspro@gmail.com>
2026-03-16 09:47:44 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,18 +3,17 @@ const path = require('path');
 
 const app = express();
 
-// Pour lire le JSON envoyé dans les requêtes
 app.use(express.json());
-
-// Rendre le dossier public accessible
 app.use('/invoices', express.static(path.join(__dirname, '../public/invoices')));
 
-// Route de test
+// Import des routes
+const invoiceRoutes = require('./routes/invoice');
+app.use('/api', invoiceRoutes);
+
 app.get('/', (req, res) => {
   res.send('Factura API est en ligne 🚀');
 });
 
-// Lancer le serveur
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Factura tourne sur http://localhost:${PORT}`);
